72b0ed8f03a3bb216eaff3ab583042b888af1dd4
Yael Tapia <90061475+yberside42@users.noreply.github.com>
2025-10-20 17:02:44 -0600

    Categories list

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+# You can add your Categories here.
+
+CATEGORIES = [
+    "Food", 
+    "Entertainment", 
+    "Healthcare", 
+    "Transport", 
+    "Subscriptions", 
+    "Education", 
+    "Pets",
+    "Gas",
+    "Others"
+]
